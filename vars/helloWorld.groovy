36fcd1ac59689fd7e8df65b14b1499939bff70e5
Shankar Balakrishna <shankarnarayanb@gmail.com>
2022-06-01 14:51:02 +0100

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,9 +1,9 @@
 def call(Map config = [:], String otherParam) {
     sh "echo Hello ${config.name}. Today is ${config.dayOfWeek}."
     sh "echo $otherParam"
-    if (otherParam.equalsIgnoreCase( "zaap" ) {
+    if (otherParam.equalsIgnoreCase( "zaap" )) {
         return true
     } else {
-        retuen false   
+        return false   
     }
 }
