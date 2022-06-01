5d64f1690987be77623bf769f729af866cf006da
Shankar Balakrishna <shankarnarayanb@gmail.com>
2022-06-01 15:53:03 +0100

    Update is_resource_available.groovy

--- vars/is_resource_available.groovy
+++ vars/is_resource_available.groovy
@@ -1,6 +1,6 @@
 def call(String otherParam) {
     sh "echo $otherParam"
-    if (otherParam.equalsIgnoreCase( "zaap" )) {
+    if (otherParam.equalsIgnoreCase( "ap-name-zaap" )) {
         return true
     } else {
         return false   
